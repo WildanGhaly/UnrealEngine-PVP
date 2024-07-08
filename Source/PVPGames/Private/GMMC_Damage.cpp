d5c0f6e70e8990fb443a6ab24923ac79c200c7be
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-07-08 11:44:17 +0700

    Add Damage Calculation Mechanism (batch of 5 files)

--- Source/PVPGames/Private/GMMC_Damage.cpp
+++ Source/PVPGames/Private/GMMC_Damage.cpp
@@ -2,4 +2,41 @@
 
 
 #include "GMMC_Damage.h"
+#include "PVPGames/CharacterAttributeSet.h"
 
+UGMMC_Damage::UGMMC_Damage() 
+{
+	DefenseDef.AttributeToCapture = UCharacterAttributeSet::GetDefenseAttribute();
+	DefenseDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	DefenseDef.bSnapshot = false;
+
+	AttackDamageDef.AttributeToCapture = UCharacterAttributeSet::GetAttackDamageAttribute();
+	AttackDamageDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Source;
+	AttackDamageDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(DefenseDef);
+	RelevantAttributesToCapture.Add(AttackDamageDef);
+}
+
+float UGMMC_Damage::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& spec) const
+{
+	const FGameplayTagContainer* SourceTags = spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Defense = 0.0f;
+	GetCapturedAttributeMagnitude(DefenseDef, spec, EvaluationParameters, Defense);
+
+	float AttackDamage = 0.0f;
+	GetCapturedAttributeMagnitude(AttackDamageDef, spec, EvaluationParameters, AttackDamage);
+
+	float damage = (AttackDamage / (AttackDamage + Defense)) * AttackDamage;
+	UE_LOG(LogTemp, Log, TEXT("Attack Damage: %f"), AttackDamage);
+	UE_LOG(LogTemp, Log, TEXT("Defense: %f"), Defense);
+	UE_LOG(LogTemp, Log, TEXT("Calculated Damage: %f"), damage);
+
+	return damage;
+}
--- Source/PVPGames/Public/GMMC_Damage.h
+++ Source/PVPGames/Public/GMMC_Damage.h
@@ -14,4 +14,11 @@ class PVPGAMES_API UGMMC_Damage : public UGameplayModMagnitudeCalculation
 {
 	GENERATED_BODY()
 	
+	UGMMC_Damage();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& spec) const override;
+
+	FGameplayEffectAttributeCaptureDefinition DefenseDef;
+	FGameplayEffectAttributeCaptureDefinition AttackDamageDef;
+	
 };

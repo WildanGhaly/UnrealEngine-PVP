cdc363550ebe4f5fbbb6b61f42eb6a2761138c12
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-07-08 08:28:36 +0700

    Set Up Gameplay Attribute on value change (batch of 5 files)

--- Source/PVPGames/CharacterAttributeSet.cpp
+++ Source/PVPGames/CharacterAttributeSet.cpp
@@ -2,6 +2,7 @@
 #include <Net/UnrealNetwork.h>
 #include "PVPGamesCharacter.h"
 #include "Math/UnrealMathUtility.h"
+#include "GameplayEffectExtension.h"
 
 UCharacterAttributeSet::UCharacterAttributeSet()
 {
@@ -22,89 +23,166 @@ void UCharacterAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, DeathCount, COND_None, REPNOTIFY_Always);
 }
 
+void UCharacterAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+    // Handle logic before an attribute changes
+    Super::PreAttributeChange(Attribute, NewValue);
+
+    if (Attribute == GetLevelAttribute())
+    {
+        // Logic to handle before the Level attribute changes
+        // For example, you might want to modify the new value based on some conditions
+    }
+}
+
+void UCharacterAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+    Super::PostGameplayEffectExecute(Data);
+
+    if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+    {
+        OnHealthChanged(Health.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetManaAttribute())
+    {
+        OnManaChanged(Mana.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+    {
+        OnStaminaChanged(Stamina.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetAttackDamageAttribute())
+    {
+        OnAttackDamageChanged(AttackDamage.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetAttackSpeedAttribute())
+    {
+        OnAttackSpeedChanged(AttackSpeed.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetDefenseAttribute())
+    {
+        OnDefenseChanged(Defense.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetMovementSpeedAttribute())
+    {
+        OnMovementSpeedChanged(MovementSpeed.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetExperienceAttribute())
+    {
+        OnExperienceChanged(Experience.GetCurrentValue());
+        // Check for level up
+        int32 CurrentLevel = static_cast<int32>(Level.GetCurrentValue());
+        if (Experience.GetCurrentValue() >= ExperienceTable[CurrentLevel])
+        {
+            LevelUp();
+        }
+    }
+    else if (Data.EvaluatedData.Attribute == GetLevelAttribute())
+    {
+        OnLevelChanged(Level.GetCurrentValue());
+        InitializeStatsFromLevel();
+    }
+    else if (Data.EvaluatedData.Attribute == GetKillCountAttribute())
+    {
+        OnKillCountChanged(KillCount.GetCurrentValue());
+    }
+    else if (Data.EvaluatedData.Attribute == GetDeathCountAttribute())
+    {
+        OnDeathCountChanged(DeathCount.GetCurrentValue());
+    }
+}
+
 void UCharacterAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Health, OldHealth);
-
-    if (Health.GetCurrentValue() <= 0)
-    {
-        HandleDeath();
-    }
+    OnHealthChanged(OldHealth);
 }
 
 void UCharacterAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Mana, OldMana);
+    OnManaChanged(OldMana);
 }
 
 void UCharacterAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Stamina, OldStamina);
+    OnStaminaChanged(OldStamina);
 }
 
 void UCharacterAttributeSet::OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackDamage, OldAttackDamage);
+    OnAttackDamageChanged(OldAttackDamage);
 }
 
 void UCharacterAttributeSet::OnRep_AttackSpeed(const FGameplayAttributeData& OldAttackSpeed)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackSpeed, OldAttackSpeed);
+    OnAttackSpeedChanged(OldAttackSpeed);
 }
 
 void UCharacterAttributeSet::OnRep_Defense(const FGameplayAttributeData& OldDefense)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Defense, OldDefense);
+    OnDefenseChanged(OldDefense);
 }
 
 void UCharacterAttributeSet::OnRep_MovementSpeed(const FGameplayAttributeData& OldMovementSpeed)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, MovementSpeed, OldMovementSpeed);
+    OnMovementSpeedChanged(OldMovementSpeed);
 }
 
 void UCharacterAttributeSet::OnRep_Experience(const FGameplayAttributeData& OldExperience)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Experience, OldExperience);
+    OnExperienceChanged(OldExperience);
 }
 
 void UCharacterAttributeSet::OnRep_Level(const FGameplayAttributeData& OldLevel)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Level, OldLevel);
+    OnLevelChanged(OldLevel);
     InitializeStatsFromLevel();
 }
 
 void UCharacterAttributeSet::OnRep_KillCount(const FGameplayAttributeData& OldKillCount)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, KillCount, OldKillCount);
+    OnKillCountChanged(OldKillCount);
 }
 
 void UCharacterAttributeSet::OnRep_DeathCount(const FGameplayAttributeData& OldDeathCount)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, DeathCount, OldDeathCount);
+    OnDeathCountChanged(OldDeathCount);
 }
 
 void UCharacterAttributeSet::HandleDeath()
 {
-    // Increment the DeathCount attribute
-    DeathCount.SetCurrentValue(DeathCount.GetCurrentValue() + 1);
-
-    // Call OnCharacterDeath on the owning character
-    AActor* OwningActor = GetOwningActor();
-    if (OwningActor)
+    ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwningActor());
+    if (OwnerCharacter)
     {
-        APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(OwningActor);
+        APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(OwnerCharacter);
         if (Character)
         {
-            Character->OnCharacterDeath();
+            Character->OnCharacterDeath_BP();
         }
     }
 }
 
 void UCharacterAttributeSet::AddExperience(float ExperiencePoints)
 {
-    Experience.SetCurrentValue(Experience.GetCurrentValue() + ExperiencePoints);
+    float OldExperience = Experience.GetCurrentValue();
+    float NewExperience = OldExperience + ExperiencePoints;
+    Experience.SetBaseValue(NewExperience);
+    Experience.SetCurrentValue(NewExperience);
 
-    while (Level.GetCurrentValue() < MaxLevel && Experience.GetCurrentValue() >= ExperienceTable[(int32)Level.GetCurrentValue()])
+    OnExperienceChanged(OldExperience);
+
+    int32 CurrentLevel = Level.GetCurrentValue();
+    if (CurrentLevel < MaxLevel && NewExperience >= ExperienceTable[CurrentLevel])
     {
         LevelUp();
     }
@@ -112,70 +190,45 @@ void UCharacterAttributeSet::AddExperience(float ExperiencePoints)
 
 void UCharacterAttributeSet::LevelUp()
 {
-    int32 CurrentLevel = (int32)Level.GetCurrentValue();
-    if (CurrentLevel >= MaxLevel)
-        return;
+    int32 CurrentLevel = Level.GetCurrentValue();
+    if (CurrentLevel < MaxLevel)
+    {
+        Level.SetBaseValue(CurrentLevel + 1);
+        Level.SetCurrentValue(CurrentLevel + 1);
+        InitializeStatsFromLevel();
+    }
+}
 
-    const FLevelStats& Stats = LevelStatsTable[CurrentLevel];
+void UCharacterAttributeSet::InitializeStatsFromLevel()
+{
+    int32 CurrentLevel = Level.GetCurrentValue();
+    if (LevelStatsDataTable)
+    {
+        FString ContextString;
+        FName RowName = FName(*FString::FromInt(CurrentLevel));
+        FLevelStats* LevelStats = LevelStatsDataTable->FindRow<FLevelStats>(RowName, ContextString);
+        if (LevelStats)
+        {
+            Health.SetBaseValue(LevelStats->Health);
+            Mana.SetBaseValue(LevelStats->Mana);
+            Stamina.SetBaseValue(LevelStats->Stamina);
+            AttackDamage.SetBaseValue(GetRandomAttackDamage(LevelStats->AttackDamage));
+            AttackSpeed.SetBaseValue(LevelStats->AttackSpeed);
+            Defense.SetBaseValue(LevelStats->Defense);
+            MovementSpeed.SetBaseValue(LevelStats->MovementSpeed);
 
-    Health.SetBaseValue(Stats.Health);
-    Health.SetCurrentValue(Stats.Health);
-
-    Mana.SetBaseValue(Stats.Mana);
-    Mana.SetCurrentValue(Stats.Mana);
-
-    Stamina.SetBaseValue(Stats.Stamina);
-    Stamina.SetCurrentValue(Stats.Stamina);
-
-    AttackDamage.SetBaseValue((Stats.AttackDamage.X + Stats.AttackDamage.Y) / 2);
-    AttackDamage.SetCurrentValue(GetRandomAttackDamage(Stats.AttackDamage));
-
-    AttackSpeed.SetBaseValue(Stats.AttackSpeed);
-    AttackSpeed.SetCurrentValue(Stats.AttackSpeed);
-
-    Defense.SetBaseValue(Stats.Defense);
-    Defense.SetCurrentValue(Stats.Defense);
-
-    MovementSpeed.SetBaseValue(Stats.MovementSpeed);
-    MovementSpeed.SetCurrentValue(Stats.MovementSpeed);
-
-    Level.SetCurrentValue(CurrentLevel + 1);
-    Experience.SetCurrentValue(Experience.GetCurrentValue() - ExperienceTable[CurrentLevel]);
-
-    // Additional logic for level up can be added here
+            Health.SetCurrentValue(LevelStats->Health);
+            Mana.SetCurrentValue(LevelStats->Mana);
+            Stamina.SetCurrentValue(LevelStats->Stamina);
+            AttackDamage.SetCurrentValue(GetRandomAttackDamage(LevelStats->AttackDamage));
+            AttackSpeed.SetCurrentValue(LevelStats->AttackSpeed);
+            Defense.SetCurrentValue(LevelStats->Defense);
+            MovementSpeed.SetCurrentValue(LevelStats->MovementSpeed);
+        }
+    }
 }
 
 float UCharacterAttributeSet::GetRandomAttackDamage(FVector2D DamageRange)
 {
     return FMath::RandRange(DamageRange.X, DamageRange.Y);
-}
-
-void UCharacterAttributeSet::InitializeStatsFromLevel()
-{
-    int32 CurrentLevel = (int32)Level.GetCurrentValue() - 1;
-    if (CurrentLevel < 0 || CurrentLevel >= MaxLevel)
-        return;
-
-    const FLevelStats& Stats = LevelStatsTable[CurrentLevel];
-
-    Health.SetBaseValue(Stats.Health);
-    Health.SetCurrentValue(Stats.Health);
-
-    Mana.SetBaseValue(Stats.Mana);
-    Mana.SetCurrentValue(Stats.Mana);
-
-    Stamina.SetBaseValue(Stats.Stamina);
-    Stamina.SetCurrentValue(Stats.Stamina);
-
-    AttackDamage.SetBaseValue((Stats.AttackDamage.X + Stats.AttackDamage.Y) / 2);
-    AttackDamage.SetCurrentValue(GetRandomAttackDamage(Stats.AttackDamage));
-
-    AttackSpeed.SetBaseValue(Stats.AttackSpeed);
-    AttackSpeed.SetCurrentValue(Stats.AttackSpeed);
-
-    Defense.SetBaseValue(Stats.Defense);
-    Defense.SetCurrentValue(Stats.Defense);
-
-    MovementSpeed.SetBaseValue(Stats.MovementSpeed);
-    MovementSpeed.SetCurrentValue(Stats.MovementSpeed);
 }
--- Source/PVPGames/CharacterAttributeSet.h
+++ Source/PVPGames/CharacterAttributeSet.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
 #include "CharacterAttributeSet.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
@@ -10,6 +11,33 @@
     GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
     GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
     GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+USTRUCT(BlueprintType)
+struct FLevelStats : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Health;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Mana;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Stamina;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FVector2D AttackDamage; // X: Min, Y: Max
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float AttackSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Defense;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MovementSpeed;
+};
 
 UCLASS()
 class PVPGAMES_API UCharacterAttributeSet : public UAttributeSet
@@ -21,132 +49,135 @@ public:
 
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-    // Health
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Attributes")
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Attributes")
+    UDataTable* LevelStatsDataTable;
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Health, Category = "Attributes")
     FGameplayAttributeData Health;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Health);
 
     UFUNCTION()
     virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
 
-    // Mana
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnHealthChanged(const FGameplayAttributeData& OldHealth);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Mana, Category = "Attributes")
     FGameplayAttributeData Mana;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Mana);
 
     UFUNCTION()
     virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
 
-    // Stamina
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnManaChanged(const FGameplayAttributeData& OldMana);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Stamina, Category = "Attributes")
     FGameplayAttributeData Stamina;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Stamina);
 
     UFUNCTION()
     virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
 
-    // Attack Damage
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AttackDamage, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnStaminaChanged(const FGameplayAttributeData& OldStamina);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_AttackDamage, Category = "Attributes")
     FGameplayAttributeData AttackDamage;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, AttackDamage);
 
     UFUNCTION()
     virtual void OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage);
 
-    // Attack Speed
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_AttackSpeed, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnAttackDamageChanged(const FGameplayAttributeData& OldAttackDamage);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_AttackSpeed, Category = "Attributes")
     FGameplayAttributeData AttackSpeed;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, AttackSpeed);
 
     UFUNCTION()
     virtual void OnRep_AttackSpeed(const FGameplayAttributeData& OldAttackSpeed);
 
-    // Defense
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Defense, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnAttackSpeedChanged(const FGameplayAttributeData& OldAttackSpeed);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Defense, Category = "Attributes")
     FGameplayAttributeData Defense;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Defense);
 
     UFUNCTION()
     virtual void OnRep_Defense(const FGameplayAttributeData& OldDefense);
 
-    // Movement Speed
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MovementSpeed, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnDefenseChanged(const FGameplayAttributeData& OldDefense);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_MovementSpeed, Category = "Attributes")
     FGameplayAttributeData MovementSpeed;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, MovementSpeed);
 
     UFUNCTION()
     virtual void OnRep_MovementSpeed(const FGameplayAttributeData& OldMovementSpeed);
 
-    // Experience
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Experience, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnMovementSpeedChanged(const FGameplayAttributeData& OldMovementSpeed);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Experience, Category = "Attributes")
     FGameplayAttributeData Experience;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Experience);
 
     UFUNCTION()
     virtual void OnRep_Experience(const FGameplayAttributeData& OldExperience);
 
-    // Level
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Level, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnExperienceChanged(const FGameplayAttributeData& OldExperience);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_Level, Category = "Attributes")
     FGameplayAttributeData Level;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, Level);
 
     UFUNCTION()
     virtual void OnRep_Level(const FGameplayAttributeData& OldLevel);
 
-    // Kill Count
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_KillCount, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnLevelChanged(const FGameplayAttributeData& OldLevel);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_KillCount, Category = "Attributes")
     FGameplayAttributeData KillCount;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, KillCount);
 
     UFUNCTION()
     virtual void OnRep_KillCount(const FGameplayAttributeData& OldKillCount);
 
-    // Death Count
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_DeathCount, Category = "Attributes")
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnKillCountChanged(const FGameplayAttributeData& OldKillCount);
+
+    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_DeathCount, Category = "Attributes")
     FGameplayAttributeData DeathCount;
     ATTRIBUTE_ACCESSORS(UCharacterAttributeSet, DeathCount);
 
     UFUNCTION()
     virtual void OnRep_DeathCount(const FGameplayAttributeData& OldDeathCount);
 
-    // Handle character's death
+    UFUNCTION(BlueprintImplementableEvent, Category = "Attributes")
+    void OnDeathCountChanged(const FGameplayAttributeData& OldDeathCount);
+
     UFUNCTION(BlueprintCallable, Category = "Attributes")
     void HandleDeath();
 
-    // Add experience and handle level up
     UFUNCTION(BlueprintCallable, Category = "Attributes")
     void AddExperience(float ExperiencePoints);
 
-    // Initialize stats based on the current level
     UFUNCTION(BlueprintCallable, Category = "Attributes")
     void InitializeStatsFromLevel();
+
+protected:
+    virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 private:
     static const int32 MaxLevel = 10;
     int32 ExperienceTable[MaxLevel] = { 500, 1000, 1600, 2300, 3100, 4000, 5200, 6500, 8000 };
-
-    struct FLevelStats
-    {
-        float Health;
-        FVector2D AttackDamage; // X: Min, Y: Max
-        float AttackSpeed;
-        float Defense;
-        float MovementSpeed;
-        float Mana;
-        float Stamina;
-    };
-
-    FLevelStats LevelStatsTable[MaxLevel] = {
-        { 2700, FVector2D(135, 170), 90, 105, 70, 100, 100 },
-        { 2900, FVector2D(138, 175), 90, 108, 70, 110, 110 },
-        { 3200, FVector2D(140, 180), 92, 110, 70, 120, 120 },
-        { 3400, FVector2D(143, 185), 92, 112, 70, 130, 130 },
-        { 3600, FVector2D(148, 190), 92, 114, 72, 140, 140 },
-        { 3700, FVector2D(153, 192), 93, 116, 72, 150, 150 },
-        { 4000, FVector2D(157, 197), 94, 119, 72, 160, 160 },
-        { 4200, FVector2D(164, 200), 95, 122, 72, 170, 170 },
-        { 4500, FVector2D(170, 210), 95, 125, 72, 200, 200 }
-    };
 
     void LevelUp();
     float GetRandomAttackDamage(FVector2D DamageRange);

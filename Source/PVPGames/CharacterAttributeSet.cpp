aec5a23a9f6c3eb4c954122540940d8a7f7c4a91
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-07-08 14:21:05 +0700

    Committing remaining 3 files

--- Source/PVPGames/CharacterAttributeSet.cpp
+++ Source/PVPGames/CharacterAttributeSet.cpp
@@ -14,6 +14,8 @@ void UCharacterAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, Mana, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, AttackDamage, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, AttackDamageMin, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, AttackDamageMax, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, AttackSpeed, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, Defense, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UCharacterAttributeSet, MovementSpeed, COND_None, REPNOTIFY_Always);
@@ -39,61 +41,90 @@ void UCharacterAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModC
 {
     Super::PostGameplayEffectExecute(Data);
 
-    if (Data.EvaluatedData.Attribute == GetHealthAttribute())
-    {
-        OnHealthChanged(Health.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetManaAttribute())
-    {
-        OnManaChanged(Mana.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
-    {
-        OnStaminaChanged(Stamina.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetAttackDamageAttribute())
-    {
-        OnAttackDamageChanged(AttackDamage.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetAttackSpeedAttribute())
-    {
-        OnAttackSpeedChanged(AttackSpeed.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetDefenseAttribute())
-    {
-        OnDefenseChanged(Defense.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetMovementSpeedAttribute())
-    {
-        OnMovementSpeedChanged(MovementSpeed.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetExperienceAttribute())
-    {
-        OnExperienceChanged(Experience.GetCurrentValue());
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
 
-        // Check for level up
-        int32 CurrentLevel = static_cast<int32>(Level.GetCurrentValue());
-        while (CurrentLevel < MaxLevel && Experience.GetCurrentValue() >= ExperienceTable[CurrentLevel-1])
+    if (Character)
+    {
+        if (Data.EvaluatedData.Attribute == GetHealthAttribute())
         {
-            float ExcessExperience = Experience.GetCurrentValue() - ExperienceTable[CurrentLevel-1];
-            LevelUp();
-            Experience.SetBaseValue(ExcessExperience);
-            Experience.SetCurrentValue(ExcessExperience);
-            CurrentLevel = static_cast<int32>(Level.GetCurrentValue());
+            OnHealthChanged(Health.GetCurrentValue());
+            Character->OnHealthChanged(Health);
         }
-    }
-    else if (Data.EvaluatedData.Attribute == GetLevelAttribute())
-    {
-        OnLevelChanged(Level.GetCurrentValue());
-        InitializeStatsFromLevel();
-    }
-    else if (Data.EvaluatedData.Attribute == GetKillCountAttribute())
-    {
-        OnKillCountChanged(KillCount.GetCurrentValue());
-    }
-    else if (Data.EvaluatedData.Attribute == GetDeathCountAttribute())
-    {
-        OnDeathCountChanged(DeathCount.GetCurrentValue());
+        else if (Data.EvaluatedData.Attribute == GetManaAttribute())
+        {
+            OnManaChanged(Mana.GetCurrentValue());
+            Character->OnManaChanged(Mana);
+        }
+        else if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+        {
+            OnStaminaChanged(Stamina.GetCurrentValue());
+            Character->OnStaminaChanged(Stamina);
+        }
+        else if (Data.EvaluatedData.Attribute == GetAttackDamageAttribute())
+        {
+            OnAttackDamageChanged(AttackDamage.GetCurrentValue());
+            Character->OnAttackDamageChanged(AttackDamage);
+        }
+        else if (Data.EvaluatedData.Attribute == GetAttackDamageMinAttribute())
+        {
+            OnAttackDamageMinChanged(AttackDamageMin.GetCurrentValue());
+            Character->OnAttackDamageMinChanged(AttackDamageMin);
+        }
+        else if (Data.EvaluatedData.Attribute == GetAttackDamageMaxAttribute())
+        {
+            OnAttackDamageMaxChanged(AttackDamageMax.GetCurrentValue());
+            Character->OnAttackDamageMaxChanged(AttackDamageMax);
+        }
+        else if (Data.EvaluatedData.Attribute == GetAttackSpeedAttribute())
+        {
+            OnAttackSpeedChanged(AttackSpeed.GetCurrentValue());
+            Character->OnAttackSpeedChanged(AttackSpeed);
+        }
+        else if (Data.EvaluatedData.Attribute == GetDefenseAttribute())
+        {
+            OnDefenseChanged(Defense.GetCurrentValue());
+            Character->OnDefenseChanged(Defense);
+        }
+        else if (Data.EvaluatedData.Attribute == GetMovementSpeedAttribute())
+        {
+            OnMovementSpeedChanged(MovementSpeed.GetCurrentValue());
+            Character->OnMovementSpeedChanged(MovementSpeed);
+        }
+        else if (Data.EvaluatedData.Attribute == GetExperienceAttribute())
+        {
+            OnExperienceChanged(Experience.GetCurrentValue());
+
+            // Check for level up
+            int32 CurrentLevel = static_cast<int32>(Level.GetCurrentValue());
+            while (CurrentLevel < MaxLevel && Experience.GetCurrentValue() >= ExperienceTable[CurrentLevel - 1])
+            {
+                float ExcessExperience = Experience.GetCurrentValue() - ExperienceTable[CurrentLevel - 1];
+                LevelUp();
+                Experience.SetBaseValue(ExcessExperience);
+                Experience.SetCurrentValue(ExcessExperience);
+                CurrentLevel = static_cast<int32>(Level.GetCurrentValue());
+            }
+
+            // Call the OnExperienceChanged function on the owning character
+            Character->OnExperienceChanged(Experience);
+        }
+        else if (Data.EvaluatedData.Attribute == GetLevelAttribute())
+        {
+            OnLevelChanged(Level.GetCurrentValue());
+            Character->OnLevelChanged(Level);
+            InitializeStatsFromLevel();
+        }
+        else if (Data.EvaluatedData.Attribute == GetKillCountAttribute())
+        {
+            OnKillCountChanged(KillCount.GetCurrentValue());
+            Character->OnKillCountChanged(KillCount);
+        }
+        else if (Data.EvaluatedData.Attribute == GetDeathCountAttribute())
+        {
+            OnDeathCountChanged(DeathCount.GetCurrentValue());
+            Character->OnDeathCountChanged(DeathCount);
+        }
     }
 }
 
@@ -101,48 +132,130 @@ void UCharacterAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealt
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Health, OldHealth);
     OnHealthChanged(OldHealth);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnHealthChanged(OldHealth);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Mana, OldMana);
     OnManaChanged(OldMana);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnManaChanged(OldMana);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Stamina, OldStamina);
     OnStaminaChanged(OldStamina);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnStaminaChanged(OldStamina);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_AttackDamage(const FGameplayAttributeData& OldAttackDamage)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackDamage, OldAttackDamage);
     OnAttackDamageChanged(OldAttackDamage);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnAttackDamageChanged(OldAttackDamage);
+    }
+}
+
+void UCharacterAttributeSet::OnRep_AttackDamageMin(const FGameplayAttributeData& OldAttackDamageMin)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackDamageMin, OldAttackDamageMin);
+    OnAttackDamageMinChanged(OldAttackDamageMin);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnAttackDamageMinChanged(OldAttackDamageMin);
+    }
+}
+
+void UCharacterAttributeSet::OnRep_AttackDamageMax(const FGameplayAttributeData& OldAttackDamageMax)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackDamageMax, OldAttackDamageMax);
+    OnAttackDamageMaxChanged(OldAttackDamageMax);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnAttackDamageMaxChanged(OldAttackDamageMax);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_AttackSpeed(const FGameplayAttributeData& OldAttackSpeed)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, AttackSpeed, OldAttackSpeed);
     OnAttackSpeedChanged(OldAttackSpeed);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnAttackSpeedChanged(OldAttackSpeed);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_Defense(const FGameplayAttributeData& OldDefense)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Defense, OldDefense);
     OnDefenseChanged(OldDefense);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnDefenseChanged(OldDefense);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_MovementSpeed(const FGameplayAttributeData& OldMovementSpeed)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, MovementSpeed, OldMovementSpeed);
     OnMovementSpeedChanged(OldMovementSpeed);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnMovementSpeedChanged(OldMovementSpeed);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_Experience(const FGameplayAttributeData& OldExperience)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Experience, OldExperience);
     OnExperienceChanged(OldExperience);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnExperienceChanged(OldExperience);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_Level(const FGameplayAttributeData& OldLevel)
@@ -150,18 +263,39 @@ void UCharacterAttributeSet::OnRep_Level(const FGameplayAttributeData& OldLevel)
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, Level, OldLevel);
     OnLevelChanged(OldLevel);
     InitializeStatsFromLevel();
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnLevelChanged(OldLevel);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_KillCount(const FGameplayAttributeData& OldKillCount)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, KillCount, OldKillCount);
     OnKillCountChanged(OldKillCount);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnKillCountChanged(OldKillCount);
+    }
 }
 
 void UCharacterAttributeSet::OnRep_DeathCount(const FGameplayAttributeData& OldDeathCount)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(UCharacterAttributeSet, DeathCount, OldDeathCount);
     OnDeathCountChanged(OldDeathCount);
+
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnDeathCountChanged(OldDeathCount);
+    }
 }
 
 void UCharacterAttributeSet::HandleDeath()
@@ -197,6 +331,14 @@ void UCharacterAttributeSet::AddExperience(float ExperiencePoints)
     // Update the experience to reflect the remaining experience after level-ups
     Experience.SetBaseValue(NewExperience);
     Experience.SetCurrentValue(NewExperience);
+
+    // Call the OnExperienceChanged function on the owning character
+    AActor* Owner = GetOwningActor();
+    APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+    if (Character)
+    {
+        Character->OnExperienceChanged(Experience);
+    }
 }
 
 
@@ -208,6 +350,14 @@ void UCharacterAttributeSet::LevelUp()
         Level.SetBaseValue(CurrentLevel + 1);
         Level.SetCurrentValue(CurrentLevel + 1);
         InitializeStatsFromLevel();
+
+        // Call the OnLevelChanged function on the owning character
+        AActor* Owner = GetOwningActor();
+        APVPGamesCharacter* Character = Cast<APVPGamesCharacter>(Owner);
+        if (Character)
+        {
+            Character->OnLevelChanged(Level);
+        }
     }
 }
 
@@ -225,6 +375,8 @@ void UCharacterAttributeSet::InitializeStatsFromLevel()
             Mana.SetBaseValue(LevelStats->Mana);
             Stamina.SetBaseValue(LevelStats->Stamina);
             AttackDamage.SetBaseValue(GetRandomAttackDamage(LevelStats->AttackDamage));
+            AttackDamageMin.SetBaseValue(LevelStats->AttackDamageMin);
+            AttackDamageMax.SetBaseValue(LevelStats->AttackDamageMax);
             AttackSpeed.SetBaseValue(LevelStats->AttackSpeed);
             Defense.SetBaseValue(LevelStats->Defense);
             MovementSpeed.SetBaseValue(LevelStats->MovementSpeed);
@@ -233,6 +385,8 @@ void UCharacterAttributeSet::InitializeStatsFromLevel()
             Mana.SetCurrentValue(LevelStats->Mana);
             Stamina.SetCurrentValue(LevelStats->Stamina);
             AttackDamage.SetCurrentValue(GetRandomAttackDamage(LevelStats->AttackDamage));
+            AttackDamageMin.SetCurrentValue(LevelStats->AttackDamageMin);
+            AttackDamageMax.SetCurrentValue(LevelStats->AttackDamageMax);
             AttackSpeed.SetCurrentValue(LevelStats->AttackSpeed);
             Defense.SetCurrentValue(LevelStats->Defense);
             MovementSpeed.SetCurrentValue(LevelStats->MovementSpeed);
@@ -248,4 +402,3 @@ float UCharacterAttributeSet::GetRandomAttackDamage(FVector2D DamageRange)
 {
     return FMath::RandRange(DamageRange.X, DamageRange.Y);
 }
-
